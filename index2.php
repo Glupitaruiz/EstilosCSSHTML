64bd52baf0880aa06e841bfb8aac9391f626ccda
Laura <106278675+Glupitaruiz@users.noreply.github.com>
2022-07-06 20:39:53 -0500

    "Unidad III. Tarea 2"

---
+++ index2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    $x = 20;
+    $y = 8;
+
+    echo $x + $y;
+
+    echo "<br>"
+      
+    ?>
+    <?php 
+
+    $x = 9.8;
+    $y = 7.6;
+
+    echo $x + $y; 
+
+    echo "<br>"
+    ?>
+
+<?php 
+
+$x = 9;
+$y = 8;
+
+echo $x * $y; 
+
+echo "<br>"
+
+?>
+
+<?php
+$var1='Laura';
+$var2=' 10';
+$texto_completo = $var1 . $var2;
+echo $texto_completo;
+
+?>
+
+ </body>
+</html> 
